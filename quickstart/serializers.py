d3d181b37776a9c3ac0d799a5f6da27325d4b54c
MarkoAdamov <markoadamov.zr@gmail.com>
2023-05-30 15:48:49 +0200

    authWithJWT

--- quickstart/serializers.py
+++ quickstart/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
 from quickstart.models import ToDo
-
-from rest_framework import serializers
-from quickstart.models import ToDo
+from django.contrib.auth import get_user_model
 
 class ToDoSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = ToDo
 		fields = ('__all__')
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
